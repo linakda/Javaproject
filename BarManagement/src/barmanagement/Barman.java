1497032bad79305b93ab400316c09bb234eee280
TheophileLct <50861637+TheophileLct@users.noreply.github.com>
2019-11-14 18:54:01 +0100

    Exemple phase 2
    Look le début de programme faudrait qu'on se calle comme ça pour les commentaires.

--- BarManagement/src/barmanagement/Barman.java
+++ BarManagement/src/barmanagement/Barman.java
@@ -6,45 +6,70 @@
 package barmanagement;
 
 /**
- *
+ * C'est classe est la classe fille de Humain.
+ * Elle possède les methodes qui permettent d'initialiser le Barman, parler avec
+ * le coco à la fin, payer un verre, offrir un verre, se présenter, la disponibilité des boissons,
+ * boire un verre, boire une boisson, commander du stock et recevoir une commande. 
  * @author Théo
  */
 public class Barman extends Humain{
     String finDePhrase;
+    
+    /**
+     * Cette methode permet de sauvegarder et d'organiser les variables du barman. 
+     * Ainsi que la fin de phrase avec le coco demandé dans le sujet.
+     * @param prenom
+     * @param nom
+     * @param sexe
+     * @param crie
+     */
     public Barman (String prenom , String nom , String sexe , String crie){
         super(prenom,nom,sexe,crie,1000);
 	this.finDePhrase = "coco";
     }
     
     /**
-     * Méthode permettant
-     * d'afficher le texte du barman
-     * @param phrase permet de stocker sous la forme d'une chaîne de caractère 
-     * @return   le texte pouvant être dit par le barman selon le cas de figure avec une nomenclature commune
-     * 
+     * Cette méthode permet d'afficher le texte du barnan
+     * @param phrase : Stock la phrase dans une chaine de caractère. 
+     * @return : Retourne la phrase du barman avec une forme commune.
     */
+    @Override
     public String parler(String phrase){
         return "<Barman> "+this.getPrenom() +" : "+ phrase + " " + this.finDePhrase;
     }
+    
+    /**
+     * Cette méthode gère le paiement du barman.
+     * @return : Retourne un boolean qui dit faux car le barman ne paie pas ses consommations. 
+     */
+    @Override
     public boolean payer(){
         return false;	
     }
 
     /**
-     * Méthode permettant de demander
-     * au barman d'offir un verre, et de réaliser cette action
-     * @param client  Va permettre de stocker l'objet ( client ) à qui le verre est offert
-     * l'addition et le taux d'alcoolemie sont modifiés en conséquences
+     * Cette méthode permet de demander au barman d'offir un verre, ainsi que de 
+     * réaliser l'action correspondante.
+     * @param client : Stock le client à qui le verre est offert l'addition
+     *                  et le taux d'alcoolemie sont modifiés en conséquences
     */
+    @Override
     public void offrir(Client client){
 	System.out.println(this.parler("Tu veux quoi "+client.getPrenom()));
 	System.out.println(client.parler("un verre de  "+client.boissonFavorite.getNom()));
 	client.alcoolemie += client.boissonFavorite.tauxAlcoolemie;
 	this.setPorteMonnaie(this.getPorteMonnaie() - client.boissonFavorite.prix );
     }
+    
+    /**
+     * Cette méthode permet au barman de se présenter. 
+     * @return : la fonction parler avec une petite présentation. 
+     */
+    @Override
     public String sePresenter(){
-        return finDePhrase;
+        return parler("Ici, c'est moi qui gère le bar.");
     }
+    
     /**
      * Méthode permettantde vérifier la disponibilité 
      * d'une boisson dans le stock du bar
@@ -58,6 +83,8 @@ public class Barman extends Humain{
 	}
 	return false;
     }
+    
+    @Override
     public void boire(Boisson consommation) {
         // TODO Auto-generated method stub	
     }
